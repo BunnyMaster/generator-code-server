1afb95c6024ad86786074d8e2d69a115056f12ca
Bunny <1319900154@qq.com>
2025-07-10 17:12:57 +0800

    :wrench: 添加跨域配置

--- src/main/java/cn/bunny/controller/WebController.java
+++ src/main/java/cn/bunny/controller/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-
+ 
     @GetMapping("/")
     public String indexPage() {
         return "redirect:/database";
